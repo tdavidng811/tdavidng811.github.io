9e09cfa6119d36cf7ffb4f41dbe45349d3d582c7
ThanhNguyen <tdavidng811@gmail.com>
2017-05-23 01:11:59 +0000

    well html

---
+++ projects/product-project/js/product.js
@@ -0,0 +1,11 @@
+/*global $ _ opspark*/ 
+$(document).ready(function () {
+  // ALL YOUR CODE GOES BELOW HERE //
+  $.getJSON('data/product.json', function(product) {
+  
+  
+  
+  
+  });
+  // ALL YOUR CODE GOES ABOVE HERE //
+});
